3ff2a135ff30db3d7558bb53bf41b130a4002b1a
ravindusw <105047448+ravindusw@users.noreply.github.com>
2025-03-08 09:46:45 +0530

    implement MongoDB connection and user model; restore login page functionality

---
+++ code-rangers/src/lib/dbconfig.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectMongo = async () => {
+  console.log("Connecting to mongo");
+  if (mongoose.connection.readyState === 1) {
+    console.log("Already connected to mongo");
+    return mongoose.connection.asPromise();
+   
+  }
+  console.log("Alrjzdnljy connected to mongo");
+  return mongoose.connect(process.env.MONGO_URL as string);
+};
+
+export default connectMongo;
